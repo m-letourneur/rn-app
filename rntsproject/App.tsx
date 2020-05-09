10808a239d456062975d3838495a0d8bb46f6573
mf-letourneur <marc.letourneur@mybraintech.com>
2020-05-09 16:27:01 +0200

    poc: write and listener on firebase collection

--- rntsproject/App.tsx
+++ rntsproject/App.tsx
@@ -1,5 +1,12 @@
-import React, { useCallback } from "react";
-import { Alert, Button, Linking, StyleSheet, View } from "react-native";
+import React, { 
+  useCallback, 
+  useState,
+  useEffect
+ } from "react";
+import { Alert, Button, Linking, StyleSheet, View, Text } from "react-native";
+
+import firestore from '@react-native-firebase/firestore';
+
 
 const supportedURL = "https://galerapagos-f5bd9.web.app/";
 
@@ -20,10 +27,75 @@ const OpenURLButton = ({ url, children }) => {
   return <Button title={children} onPress={handlePress} />;
 };
 
+const clickDataId = "clicksub-1";
+
+const IncrCounterButton = ({ children }) => {
+  const [counter, setCounter] = useState(0);
+  
+  const push = async () => {
+    firestore()
+      .collection('click-data')
+      .doc(clickDataId)
+      .set({
+        id: "clicksub",
+        name: 'Ada Lovelace',
+        age: 30,
+        counter: counter + 1,
+      })
+      .then(() => {
+        console.log('Click data added!');
+        setCounter(counter + 1);
+      });
+  }
+  
+  const handleClickButton = async () => {
+    const s = await push();
+  }
+  return (
+    <View style={styles.center}>
+      <Button title={children} onPress={handleClickButton} />
+      <Text style={{fontSize: 22, color: 'lightgreen', fontWeight: "bold"}}>Counter: {counter}</Text>
+    </View> 
+  );
+};
+
+function ClickData({ docId }) {
+  
+  const [clickData, setClickData] = useState({
+    id: "clicksub",
+    name: 'Ada Lovelace',
+    age: 30,
+    counter: 1,
+  });
+  
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('click-data')
+      .doc(docId)
+      .onSnapshot(documentSnapshot => {
+        console.log('Click data: ', documentSnapshot.data());
+        setClickData(documentSnapshot.data())
+      });
+
+    // Stop listening for updates when no longer required
+    return () => subscriber();
+  }, [docId]);
+  
+
+  return (
+    <View style={styles.center}>
+      <Text style={{fontSize: 22, color: 'blue', fontWeight: "bold"}}>Collected data for {docId}: c = {clickData ? clickData.counter : -1}</Text>
+    </View> 
+  );
+  
+}
+
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View style={[styles.center, {flex: 1}]}>
       <OpenURLButton url={supportedURL}>Open THE game</OpenURLButton>
+      <IncrCounterButton>Push to Firestore and update counter</IncrCounterButton>
+      <ClickData docId={clickDataId}/>
     </View>
   );
 };
@@ -31,5 +103,10 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  // container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  // container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  center: {
+    justifyContent: "center",
+    alignItems: 'center'
+  }
 });
